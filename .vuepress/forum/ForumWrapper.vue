859866fadfa85b678eef35ead53b77aeed3066a3
MrBBot <me@mrbbot.co.uk>
2019-06-24 15:54:29 +0100

    Display user info in new forum page

--- .vuepress/forum/ForumWrapper.vue
+++ .vuepress/forum/ForumWrapper.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <button @click="login">Login</button><br />
-    <button @click="logout">Logout</button>
+  <div class="new-forum-wrapper">
+    <button v-if="!user" @click="login">Login</button>
+    <button v-else @click="logout">Logout</button>
+    <hr />
+    <pre>{{ JSON.stringify(user, null, 2) }}</pre>
   </div>
 </template>
 
 <script>
 export default {
   name: "forum",
+  data() {
+    return {
+      user: null
+    };
+  },
   mounted() {
     import("./auth").then(({ auth }) => {
       auth.addListener("user", this.onUserChanged);
@@ -32,8 +39,18 @@ export default {
     },
     onUserChanged(user) {
       console.log("User:", user);
+      this.user = user;
       //console.log(auth);
     }
   }
 };
 </script>
+
+<style lang="sass">
+.new-forum-wrapper
+  max-width: 800px
+  margin: 0 auto
+  padding: 1rem
+  pre
+    overflow-x: auto
+</style>
