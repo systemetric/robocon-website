d499bb82b992cb45b996f40227824d85aa939aab
MrBBot <me@mrbbot.co.uk>
2019-06-29 16:16:48 +0100

    Add more Quill modules

--- .vuepress/forum/editor/QuillEditor.vue
+++ .vuepress/forum/editor/QuillEditor.vue
@@ -16,8 +16,23 @@ export default {
   mounted() {
     import("./quill").then(quillModule => {
       const Quill = quillModule.default;
+      window.Quill = Quill;
       const editor = new Quill(this.$refs.editor, {
-        theme: "snow"
+        theme: "snow",
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, 3, false] }],
+            ["bold", "italic", "underline"],
+            [
+              { color: [] },
+              "link",
+              "code",
+              { script: "sub" },
+              { script: "super" }
+            ],
+            ["blockquote", "code-block"]
+          ]
+        }
       });
       editor.root.innerHTML = this.value;
       editor.on("text-change", () => {
--- .vuepress/forum/editor/quill.js
+++ .vuepress/forum/editor/quill.js
@@ -3,16 +3,32 @@ import Quill from "quill/core";
 import Toolbar from "quill/modules/toolbar";
 import Snow from "quill/themes/snow";
 
+import Header from "quill/formats/header";
+
 import Bold from "quill/formats/bold";
 import Italic from "quill/formats/italic";
-import Header from "quill/formats/header";
+import Underline from "quill/formats/underline";
+
+import { ColorStyle } from "quill/formats/color";
+import Link from "quill/formats/link";
+import Script from "quill/formats/script";
+
+import Blockquote from "quill/formats/blockquote";
+import CodeBlock, { Code } from "quill/formats/code";
 
 Quill.register({
   "modules/toolbar": Toolbar,
   "themes/snow": Snow,
+  "formats/header": Header,
   "formats/bold": Bold,
   "formats/italic": Italic,
-  "formats/header": Header
+  "formats/underline": Underline,
+  "formats/color": ColorStyle,
+  "formats/link": Link,
+  "formats/script": Script,
+  "formats/blockquote": Blockquote,
+  "formats/code-block": CodeBlock,
+  "formats/code": Code
 });
 
 export default Quill;
