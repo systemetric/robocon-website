aa71c46991455b1142c27269937afaec3a07e3f1
MrBBot <me@mrbbot.co.uk>
2019-06-30 16:22:58 +0100

    Favicon that looks good with dark browser themes

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -96,7 +96,39 @@ module.exports = {
   title: "RoboCon",
   head: [
     ["script", { src: "/prism.js" }],
-    ['link', { rel: 'icon', href: '/favicon.png' }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      }
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0094ff" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#0094ff" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
     [
       "script",
       {
