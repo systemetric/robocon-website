21ef97b213483b34c4e3ed4299b6755032eeeb78
MrBBot <me@mrbbot.co.uk>
2019-07-19 18:35:26 +0100

    Fix videos

--- .vuepress/forum/components/editor/theme.js
+++ .vuepress/forum/components/editor/theme.js
@@ -154,4 +154,27 @@ CustomSnowTooltip.TEMPLATE = [
   '<a class="ql-remove"></a>'
 ].join("");
 
+function extractVideoUrl(url) {
+  let match =
+    url.match(
+      /^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtube\.com\/watch.*v=([a-zA-Z0-9_-]+)/
+    ) ||
+    url.match(/^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtu\.be\/([a-zA-Z0-9_-]+)/);
+  if (match) {
+    return (
+      (match[1] || "https") +
+      "://www.youtube.com/embed/" +
+      match[2] +
+      "?showinfo=0"
+    );
+  }
+  if ((match = url.match(/^(?:(https?):\/\/)?(?:www\.)?vimeo\.com\/(\d+)/))) {
+    // eslint-disable-line no-cond-assign
+    return (
+      (match[1] || "https") + "://player.vimeo.com/video/" + match[2] + "/"
+    );
+  }
+  return url;
+}
+
 export default CustomSnowTheme;
