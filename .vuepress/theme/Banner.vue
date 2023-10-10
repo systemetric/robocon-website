e63f32a21ca36b05365d1cef8fd8d55ec5b66a62
tymek <88202578+tymeklew@users.noreply.github.com>
2023-10-10 17:23:03 +0100

    Added banner and moved cookie down (#194)
    * Added banner and moved cookie down
    
    * Update Home.vue
    
    Fixed error where whitespace appeared when dismissing cookie
    
    ---------
    
    Co-authored-by: Tymek Lewandowski <tymek@tymeks-air.lan>

---
+++ .vuepress/theme/Banner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="banner">
+    <h1>
+      RoboCon 2024 is Live!
+      <a
+        href="mailto:dmassey@hillsroad.ac.uk,msmalley@hillsroad.ac.uk?subject=RoboCon%202024%20Signup&body=I%20am%20registering%20my%20interest%20in%20RoboCon%202024.%0D%0A%0D%0ASchool%3A%0D%0ATeacher%20name(s)%3A%0D%0AContact%3A%0D%0ACan%20I%20attend%20kick-off%20(on%2028th%20Oct)%3F%20%3A%0D%0AApproximate%20team%20size%3A%0D%0AI%20have%20these%20questions%20about%20RoboCon%20%3A%0D%0A%0D%0A%0D%0A%0D%0A%0D%0A"
+      >
+        Join Now
+      </a>
+    </h1>
+  </div>
+</template>
